26361dbfd52fad348357e11bfded8e77a3c13677
ohoCoding <youngho7482@gmail.com>
2025-01-28 23:29:43 +0900

    feat: :sparkles: Global 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        batang: ["IropkeBatangM", "serif"], // 'batang' 클래스로 사용 가능
+      },
+    },
   },
   plugins: [],
 };
