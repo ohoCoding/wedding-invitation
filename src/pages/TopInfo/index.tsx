959750960220a3be1d475e487a2901c6fb29db1e
ohoCoding <youngho7482@gmail.com>
2025-02-01 21:56:57 +0900

    refactor: :recycle: 컴포넌트 분리 적용 & pt-10 만 적용

--- src/pages/TopInfo/index.tsx
+++ src/pages/TopInfo/index.tsx
@@ -1,43 +1,27 @@
-import { CardContent } from '@/components/ui/card';
-import mainImage from '@/assets/images/image.png';
-import { loveMessages } from '@/constants/lovemessage';
+import { CardContent } from "@/components/ui/card";
+import WeddingTitle from "@/pages/TopInfo/components/WeddingTitle";
+import CoupleName from "@/pages/TopInfo/components/CoupleName";
+import MainImage from "@/pages/TopInfo/components/MainImage";
+import WeddingInfo from "@/pages/TopInfo/components/WeddingInfo";
+import Lovemessage from "@/pages/TopInfo/components/LoveMessage";
 
 const TopInfo = () => {
   return (
-    <CardContent className="p-6 text-center">
-      {/* 결혼식 타이틀 */}
-      <h3 className="text-sm tracking-widest text-gray-500 uppercase">
-        THE WEDDING OF
-      </h3>
+    <CardContent className="pt-10 text-center">
+      {/* 웨딩 타이틀 (웨딩 폰트 적용) */}
+      <WeddingTitle />
 
       {/* 신랑 & 신부 이름 */}
-      <h1 className="mt-2 text-2xl text-gray-900">
-        이동윤 <span className="text-gray-500">그리고</span> 이하나
-      </h1>
+      <CoupleName />
 
-      {/* 결혼식 이미지 */}
-      <div className="mt-4">
-        <img
-          src={mainImage}
-          alt="Wedding"
-          className="w-full rounded-lg shadow-md"
-        />
-      </div>
+      {/* 결혼식 이미지 + 비디오 */}
+      <MainImage />
 
       {/* 러브 메시지 */}
-      <div className="flex text-center items-center justify-center leading-[2.2rem] text-[1rem] tracking-normal whitespace-pre">
-        {loveMessages.map((message, index) => (
-          <p key={index} className="mt-4 text-lg text-gray-600">
-            {message.value}
-          </p>
-        ))}
-      </div>
+      <Lovemessage />
 
       {/* 날짜 및 장소 */}
-      <div className="mt-4 text-sm text-gray-700">
-        <p>📅 2025년 6월 28일 토요일 오후 1시 30분</p>
-        <p>📍 아펠가모 광화문 B2 로스타노홀</p>
-      </div>
+      <WeddingInfo />
     </CardContent>
   );
 };
