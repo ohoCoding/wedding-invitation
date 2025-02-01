6d585ff38b0f8ca1a057457d6baf7b3032f92862
ohoCoding <youngho7482@gmail.com>
2025-02-01 21:56:06 +0900

    feat: :sparkles: 컴포넌트 분리 & font-parisienne 설정 & 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
       fontFamily: {
         default: "var(--font-default)", // 기본 폰트
         wedding: "var(--font-wedding)", // 웨딩 타이틀 폰트
+        parisienne: "var(--font-parisienne)", // 파리지엔 타이틀 폰트
       },
       colors: {
         "margin-bg": "var(--margin-bg-color)", // 마진 배경 색상
