e86cd37e41c26abcb9e4f07e46bc4cbccecb8a8d
ohoCoding <youngho7482@gmail.com>
2025-02-01 21:12:15 +0900

    feat: :sparkles: Tailwind 설정에서 기본폰트, 웨딩 타이틀 전용폰트 , 배경색 등록

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        batang: ['Iropke Batang', 'serif'], // 'batang' 클래스로 사용 가능
+        default: "var(--font-default)", // 기본 폰트
+        wedding: "var(--font-wedding)", // 웨딩 타이틀 폰트
+      },
+      colors: {
+        "margin-bg": "var(--margin-bg-color)", // 마진 배경 색상
       },
     },
   },
