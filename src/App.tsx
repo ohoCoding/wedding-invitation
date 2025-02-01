12db868a14df3b0769d2c890a014c042252188ab
ohoCoding <youngho7482@gmail.com>
2025-02-01 21:12:42 +0900

    feat: :sparkles: bg-margin-bg 를 App.tsx에 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TopInfo from "@/pages/TopInfo";
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-full bg-gray-100">
+    <div className="flex items-center justify-center h-full bg-margin-bg">
       {/* 초대장 카드 */}
       <Card className="w-full h-full max-w-md bg-white shadow-lg rounded-xl">
         <TopInfo />
