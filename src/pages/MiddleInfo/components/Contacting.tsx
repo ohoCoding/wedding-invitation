cead338d784ff4af41d72a23f1b8b09f326ad1d4
이어진 <slimin92@naver.com>
2025-02-02 22:09:47 +0900

    feat: :sparkles: 인용문, 초대 메세지, 연락하기 컴포넌트 구현
    - 구분선 추가
    - 인용문 컴포넌트 구현
    - 초대 메세지 컴포넌트 구현
    - 미들 이미지 추가
    - 연락하기 버튼 추가

---
+++ src/pages/MiddleInfo/components/Contacting.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@/components/ui/button';
+
+const Contacting = () => {
+  return (
+    <>
+      {/* 신랑측, 신부측 이름 */}
+      <div className="py-4 leading-9">
+        <div>
+          <span className="text-lg">이원배ㆍ강경희</span>
+          <span className="text-sm mx-2 text-[#8a857f]">
+            <span>의</span>
+            <span className="ml-1">아들</span>
+          </span>
+          <span className="text-lg">도현</span>
+        </div>
+        <div>
+          <span className="text-lg">서동명ㆍ권숙이</span>
+          <span className="text-sm mx-2 text-[#8a857f]">
+            <span>의</span>
+            <span className="mx-2">딸</span>
+          </span>
+          <span className="text-lg">정현</span>
+        </div>
+      </div>
+
+      {/* 연락하기 버튼 */}
+      <div>
+        <Button variant="outline" size="custom">
+          연락하기
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Contacting;
