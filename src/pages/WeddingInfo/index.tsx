f2a096ba2ce451d2e803206cf9620ea6df6e8d4d
ohoCoding <youngho7482@gmail.com>
2025-02-07 17:11:13 +0900

    refactor: :recycle: 간격 수정

--- src/pages/WeddingInfo/index.tsx
+++ src/pages/WeddingInfo/index.tsx
@@ -4,7 +4,7 @@ import { guideInfo } from '@/constants/guideInfo';
 
 const GuideInfo = () => {
   return (
-    <div className="w-full max-w-md mx-auto mt-10">
+    <div className="w-full max-w-md px-6 mx-auto mt-2">
       <h2 className="mb-4 text-lg font-semibold text-center">
         예식정보 및 안내사항
       </h2>
@@ -15,7 +15,7 @@ const GuideInfo = () => {
             <TabsTrigger
               key={tab.value}
               value={tab.value}
-              className="m-1 border border-transparent rounded-t-lg px-4 py-2 text-gray-500 data-[state=active]:bg-white data-[state=active]:text-gray-900 data-[state=active]:border-gray-500 data-[state=active]:border-b-0 "
+              className="border  rounded-t-lg px-3 py-3 bg-gray-100  text-gray-500 data-[state=active]:bg-white data-[state=active]:text-gray-900 data-[state=active]:border-gray-200 data-[state=active]:border-b-0 "
             >
               {tab.label}
             </TabsTrigger>
@@ -25,8 +25,8 @@ const GuideInfo = () => {
         {/* 탭 컨텐츠 */}
         {guideInfo.map((tab) => (
           <TabsContent key={tab.value} value={tab.value} className="mt-0">
-            <Card className="border border-gray-200 shadow-sm ">
-              <CardContent className="p-4 text-center text-black ">
+            <Card className="border border-gray-200 rounded-bl-lg rounded-br-lg shadow-sm">
+              <CardContent className="p-5 text-center text-black ">
                 {tab.content.split('\n').map((line, index) => (
                   <div key={index}>
                     {line}
