19e1953bf2b8b8da6e0ed202a08da011caf3c6dd
ohoCoding <youngho7482@gmail.com>
2025-02-14 22:20:03 +0900

    fix: :bug: 신부측 계좌번호 위치 수정

--- src/constants/account.ts
+++ src/constants/account.ts
@@ -20,14 +20,14 @@ export const groomAccount: AccountType[] = [
 
 export const brideAccount: AccountType[] = [
   {
-    name: '권숙이',
-    bank: '농협',
-    accountNumber: '352-1019-9362-03',
-  },
-  {
     name: '서동명',
     bank: '농협',
     accountNumber: '352-0900-0967-63',
+  },
+  {
+    name: '권숙이',
+    bank: '농협',
+    accountNumber: '352-1019-9362-03',
   },
   {
     name: '서정현',
